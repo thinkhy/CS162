42c9ffdf9c374470785b1cfea2af1aeba55cd766
thinkhy <think.hy@gmail.com>
2014-04-21 23:22:34 +0800

    initialize test case mulprocess.c

--- nachos/proj2/run.sh
+++ nachos/proj2/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-java nachos.machine.Machine $*
+#java nachos.machine.Machine $*
 #java nachos.machine.Machine -x exittest.coff -d ma
-#java nachos.machine.Machine -x exectest.coff  -d ma
+java nachos.machine.Machine -x exectest.coff  -d ma
 #java nachos.machine.Machine -x sh.coff  
+
+
---
+++ nachos/test/testprj2/mulprocess.c
@@ -0,0 +1,17 @@
+/*prolog****************************************************************
+ *
+ * cname:  mulprocess.c
+ * desc:   test cases for CS162 project 2: support multiprogramming
+ * author: thinkhy
+ * tccall: java nachos.machine.Machine -x mulprocess.coff  -# varnum
+ * k
+ * env:    nachos 5.0j 
+ * compile:test/make
+ * Change activity:
+ *   $BC,EPT     4/21/2014 - initial release
+ **********************************************************************/
+int main(int argc, char *argv[]) { 
+
+    return 0;
+}
+

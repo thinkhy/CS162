1e99769a23c6a6cf6f35096ae2abf9eda4699c44
thinkhy <think.hy@gmail.com>
2014-04-22 23:33:15 +0800

    make test/testprj2/mulprocess.c

--- nachos/test/testprj2/mulprocess.c
+++ nachos/test/testprj2/mulprocess.c
@@ -12,6 +12,21 @@
  **********************************************************************/
 int main(int argc, char *argv[]) { 
 
+/******************************************************************************************
+ *
+ * Variations for Project 2 Task III: Implement the system calls for process management
+ *
+ * Var 0 : tests that your syscall exit finishes the thread of the process immediately
+ * Var 1 : runs exec multiple times and checks each child gets unique PID
+ * Var 2 : tests your syscall join to a childj
+ * Var 3 : tests exec with error arguments (e.g. bad file name)
+ * Var 4 : tests your syscall join to a non-child
+ * Var 5 : tests your syscall join to a child that caused unhandled exception
+ * Var 6 : tests that your exit syscall releases all resources
+ *
+ ******************************************************************************************/
+
+
     return 0;
 }
 

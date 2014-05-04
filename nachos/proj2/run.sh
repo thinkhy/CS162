d2b72dbff9fd24f21f26f03e01a36077f3416235
thinkhy <think.hy@gmail.com>
2014-05-04 23:42:10 +0800

    Try to get out of java.lang.RuntimePermission createClassLoader

--- nachos/proj2/run.sh
+++ nachos/proj2/run.sh
@@ -2,7 +2,7 @@
 
 #java nachos.machine.Machine $*
 #java nachos.machine.Machine -x mulprocess.coff -#1 -d ma
-java nachos.machine.Machine -x isprmgr.coff   -d a
+java nachos.machine.Machine -x isprmgr.coff   -d as
 #java nachos.machine.Machine -x exittest.coff -d ma
 #java nachos.machine.Machine -x exectest.coff  -d ma
 #java nachos.machine.Machine -x sh.coff  
